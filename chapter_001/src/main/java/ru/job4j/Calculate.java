735e20805ee5d7d4ddb523dafec06210bd040200
Aleksey Slivko <sllexa@yandex.ru>
2017-04-16 12:15:37 +0300

    Анализ исходного кода

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -12,7 +12,7 @@ public class Calculate {
      * Main. Старт нашей программы.
      * @param args - args.
      */
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
 }
