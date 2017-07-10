2b7c190ebf2db75bfd75b012eb66f6053ed521e4
Aleksey Slivko <sllexa@yandex.ru>
2017-07-10 16:51:16 +0300

    3. Перекрывать hashCode [#1003]

--- chapter_005/src/test/java/ru/job4j/pro/map/UserTest.java
+++ chapter_005/src/test/java/ru/job4j/pro/map/UserTest.java
@@ -2,6 +2,8 @@ package ru.job4j.pro.map;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,17 +18,23 @@ import java.util.Map;
 public class UserTest {
     /**
      * Test create and add users in HashMap.
-     * Methods hashCode and equals not overridden.
+     * Method hashCode override.
+     * Method equals not override.
      */
     @Test
     public void whenCreateTwoObjectAndAddInHashMapThenTwoUsers() {
         User firstUser = new User("Max", 2, new GregorianCalendar(1990, 0, 20));
         User secondUser = new User("Max", 2, new GregorianCalendar(1990, 0, 20));
 
+        System.out.println("hashCode first user = " + firstUser.hashCode());
+        System.out.println("hashCode second user = " + secondUser.hashCode());
+
         Map<User, String> map = new HashMap<>();
         map.put(firstUser, "first");
         map.put(secondUser, "second");
 
         System.out.println(map);
+
+        assertThat(firstUser.hashCode(), is(secondUser.hashCode()));
     }
 }
