f93a712056c81c1d46bec429ad7ad772ecc076a7
Aleksey Slivko <sllexa@yandex.ru>
2018-07-16 19:24:01 +0300

    (Исправления)

--- chapter_005/src/test/java/ru/job4j/pro/set/ArraySetContainerTest.java
+++ chapter_005/src/test/java/ru/job4j/pro/set/ArraySetContainerTest.java
@@ -20,22 +20,17 @@ public class ArraySetContainerTest {
     @Test
     public void whenAddElementsThenGetWithIterator() {
         ArraySetContainer<Integer> set = new ArraySetContainer<>(4);
-        //Integer[] arrInt = {100, 200, 300, 400};
         set.add(1);
         set.add(2);
         set.add(3);
         set.add(4);
-        /*for (Integer ival : arrInt) {
-            set.add(ival);
-        }*/
 
         Integer[] expected = {1, 2, 3, 4};
         Iterator itr = set.iterator();
-        assertThat(itr.next(), is(4));
-/*        int ind = 0;
 
+        int ind = 0;
         while (itr.hasNext()) {
             assertThat(itr.next(), is(expected[ind++]));
-        }*/
+        }
     }
 }
