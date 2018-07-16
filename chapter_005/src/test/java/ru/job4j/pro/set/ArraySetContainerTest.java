fddfea01b74e711202adbc6fdf0762aca83bd630
Aleksey Slivko <sllexa@yandex.ru>
2018-07-16 18:01:54 +0300

    Исправления

--- chapter_005/src/test/java/ru/job4j/pro/set/ArraySetContainerTest.java
+++ chapter_005/src/test/java/ru/job4j/pro/set/ArraySetContainerTest.java
@@ -1,7 +1,7 @@
 package ru.job4j.pro.set;
 
 import org.junit.Test;
-
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
 import static org.hamcrest.core.Is.is;
@@ -20,18 +20,23 @@ public class ArraySetContainerTest {
      */
     @Test
     public void whenAddElementsThenGetWithIterator() {
-        ArraySetContainer<Integer> set = new ArraySetContainer<>(2);
-        Integer[] arrInt = {100, 200, 200, 300, 400};
-
-        for (Integer ival : arrInt) {
+        ArraySetContainer<Integer> set = new ArraySetContainer<>(4);
+        //Integer[] arrInt = {100, 200, 300, 400};
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        /*for (Integer ival : arrInt) {
             set.add(ival);
-        }
+        }*/
 
-        Integer[] expected = {100, 200, 300, 400};
+        Integer[] expected = {1, 2, 3, 4};
         Iterator itr = set.iterator();
-        int ind = 0;
+        assertThat(itr.next(), is(4));
+/*        int ind = 0;
+
         while (itr.hasNext()) {
             assertThat(itr.next(), is(expected[ind++]));
-        }
+        }*/
     }
 }
