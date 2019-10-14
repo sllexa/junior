50b1816274a1cdd602a09855610fef4acf1139d8
Aleksei Slivko <sllexa@mail.ru>
2019-10-14 19:09:43 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260] - изменения

--- chapter_003/src/main/java/ru/job4j/lite/bank/Bank.java
+++ chapter_003/src/main/java/ru/job4j/lite/bank/Bank.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * Class Bank.
@@ -19,7 +17,6 @@ public class Bank {
      * map with key - user and value - list of user accounts.
      */
     private Map<User, List<Account>> map = new HashMap<>();
-    private boolean acc;
 
     /**
      * Add client to bank.
